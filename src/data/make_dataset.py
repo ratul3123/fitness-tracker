5fa0dd2b908f3ee62f82fb16a42793f40123a9ae
Shahriar Ratul <ratul3123@gmail.com>
2026-05-08 22:27:23 +0600

    fix column order in merged dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -145,9 +145,16 @@ data_merged = pd.concat([acc_df.iloc[:,:3], gyr_df], axis=1)
 data_merged.dropna()
 
 data_merged.columns = [
-    "acc_x", "acc_y", "acc_z",
-    "gyr_x", "gyr_y", "gyr_z",
-    "label", "category", "participant", "set"
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyr_x",
+    "gyr_y",
+    "gyr_z",
+    "participant",
+    "label",
+    "category",
+    "set",
 ]
 
 # --------------------------------------------------------------
@@ -164,9 +171,9 @@ sampling = {
     "gyr_x": "mean",
     "gyr_y": "mean",
     "gyr_z": "mean",
+    "participant": "last",
     "label": "last",
     "category": "last",
-    "participant": "last",
     "set": "last"
 }
 
